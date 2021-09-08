cdab52bad512a492ab32a595fef8cad43ca89767
mcjordan2002 <mcjordan2002@gmail.com>
2021-09-08 20:23:21 +0100

    add two-sections

--- src/Main.js
+++ src/Main.js
@@ -3,6 +3,7 @@ import './Main.css'
 import Header from "./Components/Header";
 import {Col, Container, Row} from "reactstrap";
 import MyMe from "./Assets/me.jpg";
+import ResumeFr from "./Assets/Resume-fr.pdf";
 
 const Main = () => {
     return (
@@ -125,7 +126,7 @@ const Main = () => {
                     <h4 className="numbered-heading">
                         Projects
                     </h4>
-                    <div className="row row-cols-1 row-cols-md-3">
+                    <div className="row row-cols-1 row-cols-md-2 p-5">
                         <div className="col mb-4">
                             <div className="card p-2">
                                 <div className="top-card">
@@ -136,14 +137,100 @@ const Main = () => {
                                     </svg>
                                 </div>
                                 <div className="card-body">
-                                    <h5 className="card-title fw-bold">Card title</h5>
-                                    <p className="card-text description">svvsbsbdgbgdbdb</p>
-                                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                                    <h5 className="card-title fw-bold"><a href="https://github.com/mcjordan2002/B-DEV-DASHBOARD-2020">Dashboard</a></h5>
+                                    <p className="card-text description">
+                                        The purpose of this project is to familiarize us with a software platform that we choose between Java, .NET, node.js through the creation of a dashboard
+
+                                        So we need to implement a web app that work like Netvibes.
+                                    </p>
+                                    <p className="card-text"><small className="text-muted">Node.js Docker MongoDb Materialize API</small></p>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col mb-4">
+                            <div className="card p-2">
+                                <div className="top-card">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
+                                         className="bi bi-folder" viewBox="0 0 16 16">
+                                        <path
+                                            d="M.54 3.87.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31zM2.19 4a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4H2.19zm4.69-1.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707z"/>
+                                    </svg>
+                                    <a href="https://github.com/mcjordan2002/corona_autocontrol_app">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
+                                             className="bi bi-github" viewBox="0 0 16 16">
+                                            <path
+                                                d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+                                        </svg>
+                                    </a>
+                                </div>
+                                <div className="card-body">
+                                    <h5 className="card-title fw-bold"><a href="https://epitech.bj/covid-auto/">Corona auto-control</a></h5>
+                                    <p className="card-text description">
+                                        A project allowing users to test themselves for covid symptoms with a quick test of 15 questions lasting on average 40s.
+                                        And giving them at the end advice to take according to their condition
+                                    </p>
+                                    <p className="card-text"><small className="text-muted">Html Css Javascript Materialize</small></p>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col mb-4">
+                            <div className="card p-2">
+                                <div className="top-card">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
+                                         className="bi bi-folder" viewBox="0 0 16 16">
+                                        <path
+                                            d="M.54 3.87.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31zM2.19 4a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4H2.19zm4.69-1.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707z"/>
+                                    </svg>
+                                    <a href="https://github.com/mcjordan2002/corona_autocontrol_app">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
+                                             className="bi bi-github" viewBox="0 0 16 16">
+                                            <path
+                                                d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+                                        </svg>
+                                    </a>
+                                </div>
+                                <div className="card-body">
+                                    <h5 className="card-title fw-bold"><a href="https://epitech.bj/covid-auto/">Corona auto-control</a></h5>
+                                    <p className="card-text description">
+                                        A project on the theme Past Present Future that was to be realized in a weekend in all Epitech Campus. <br/><br/><br/>
+                                    </p>
+                                    <p className="card-text"><small className="text-muted">Html Css Javascript</small></p>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col mb-4">
+                            <div className="card p-2">
+                                <div className="top-card">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor"
+                                         className="bi bi-folder" viewBox="0 0 16 16">
+                                        <path
+                                            d="M.54 3.87.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31zM2.19 4a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4H2.19zm4.69-1.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707z"/>
+                                    </svg>
+                                </div>
+                                <div className="card-body">
+                                    <h5 className="card-title fw-bold"><a href="">Area</a></h5>
+                                    <p className="card-text description">
+                                        A project where the software platform that you have chosen (Java, .NET,node.js) through the creation of a business application.To do this, you must implement a software suite that functions similar to that of IFTTT and/or Zapier.
+                                    </p>
+                                    <p className="card-text"><small className="text-muted">Node.js Docker MongoDb Materialize Flutter</small></p>
 
                                 </div>
                             </div>
                         </div>
                     </div>
+                </section>
+
+                <section className="contact container p-3" id="contact">
+                    <Container className="center-align">
+                        <h6>04. What’s Next?</h6>
+                        <h1 className="fw-bold">Get in touch</h1>
+                        <p className="h6"> I’m currently looking for any new opportunities, my inbox is always open.
+                            Whether you have a question or just want to say hi, I’ll try my best to get back to you!</p>
+                    </Container>
+                    <button type="button" className="btn btn-outline-primary mt-5"><a href="mailto:jordan.vitou@epitech.eu">Say hello</a></button>
                 </section>
             </div>
 
